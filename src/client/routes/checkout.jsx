b9f98b023c693f8224c1414efead9bb8548305dd
lugrao <elugrao@gmail.com>
2024-02-29 16:53:23 -0300

    add checkout route

---
+++ src/client/routes/checkout.jsx
@@ -0,0 +1,12 @@
+import "./checkout.css";
+import NavBar from "../components/NavBar";
+import CheckoutForm from "../components/CheckoutForm";
+
+export default function Checkout() {
+  return (
+    <>
+      <NavBar />
+      <CheckoutForm />
+    </>
+  );
+}
