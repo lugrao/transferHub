4bc63a88f274e01342fa06b2913daaef3d59988c
lugrao <elugrao@gmail.com>
2024-02-29 13:00:44 -0300

    add search route as root's child

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Root from "./routes/root";
+import Search from "./routes/search";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "search",
+        element: <Search />,
+      },
+    ],
   },
 ]);
 
