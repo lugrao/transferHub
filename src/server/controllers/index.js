9a6b211f98852f1ca336c9f5ee170e2809702a61
lugrao <elugrao@gmail.com>
2024-03-05 16:03:24 -0300

    add countries endpoint

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,8 +1,33 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const getHotelbedsData = (url) =>
+  fetch(url, {
+    headers: {
+      "Api-Key": process.env.API_KEY,
+    },
+  });
+
 const controllers = {
   getTest: (req, res) => {
-    res.send(JSON.stringify({
-        message: "AGUANTE MESSI."
-    }));
+    res.send(
+      JSON.stringify({
+        message: "AGUANTE MESSI.",
+      })
+    );
+  },
+  getCountries: async (req, res) => {
+    try {
+      const response = await getHotelbedsData(
+        "https://api.test.hotelbeds.com/transfer-cache-api/1.0/locations/countries?fields=ALL&language=ES&offset=1&limit=1000"
+      );
+      const data = await response.json();
+      return res.send(data);
+    } catch (e) {
+      console.error(e);
+      return res.send(e.message);
+    }
   },
 };
 
