8642f0d6cafd6adf42a43016fecbd9a61b530aaa
lugrao <elugrao@gmail.com>
2024-03-05 16:04:49 -0300

    add countries endpoint

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -4,6 +4,7 @@ import controllers from "../../controllers/index.js";
 const router = express.Router();
 
 router.get("/test", controllers.getTest);
+router.get("/countries", controllers.getCountries);
 
 const api = router;
 
