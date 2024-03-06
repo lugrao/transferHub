053852b1c1fa6204d37e4233b3821db56cacd8da
lugrao <elugrao@gmail.com>
2024-03-06 15:39:42 -0300

    replace basic input with google autocomplete input

--- src/client/components/SearchPanel.jsx
+++ src/client/components/SearchPanel.jsx
@@ -1,5 +1,8 @@
 import "./SearchPanel.css";
 import { Link } from "react-router-dom";
+import Autocomplete from "react-google-autocomplete";
+
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 export default function SearchPanel() {
   return (
@@ -7,33 +10,54 @@ export default function SearchPanel() {
       <form action="">
         <div className="form-field">
           <label htmlFor="origin">Origen</label>
-          <input type="text" name="origin" id="origin" />
+          <Autocomplete
+            name="origin"
+            id="origin"
+            apiKey={GOOGLE_API_KEY}
+            onPlaceSelected={(place) => {
+              console.log(place);
+              console.log(place.geometry.location.lat());
+              console.log(place.geometry.location.lng());
+            }}
+            options={{ types: [] }}
+            placeholder=""
+          />
         </div>
-
         <div className="form-field">
           <label htmlFor="destination">Destino</label>
-          <input type="text" name="destination" id="destination" />
+          <Autocomplete
+            name="destination"
+            id="destination"
+            apiKey={GOOGLE_API_KEY}
+            onPlaceSelected={(place) => {
+              console.log(place);
+              console.log(place.geometry.location.lat());
+              console.log(place.geometry.location.lng());
+            }}
+            options={{ types: [] }}
+            placeholder=""
+          />
         </div>
-
         <div className="form-field">
           <label htmlFor="date">Fecha</label>
           <input type="date" name="date" id="date" />
         </div>
-
         <div className="form-field">
           <label htmlFor="time">Hora</label>
           <input type="time" name="time" id="time" />
         </div>
-
         <div className="form-field">
           <label htmlFor="passengers">Pasajeros</label>
           <select name="passengers" id="passengers">
             {Array.from({ length: 15 }, (_, i) => i + 1).map((i) => {
-              return <option value={i} key={i}>{i}</option>;
+              return (
+                <option value={i} key={i}>
+                  {i}
+                </option>
+              );
             })}
           </select>
         </div>
-
         <Link to={`/search`}>
           <input className="button" type="submit" value="Buscar" />
         </Link>
