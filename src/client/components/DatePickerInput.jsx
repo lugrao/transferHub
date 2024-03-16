f61f6f69ce0371a6206765b076007e5466b73a99
lugrao <elugrao@gmail.com>
2024-03-16 13:46:26 -0300

    add DatePickerInput component

---
+++ src/client/components/DatePickerInput.jsx
@@ -0,0 +1,18 @@
+import "./DatePickerInput.css";
+import { DatePicker } from "antd";
+
+export default function DatePickerInput() {
+  return (
+    <div className="date-picker-input-wrapper">
+      <label htmlFor="date">Fecha</label>
+      <DatePicker
+        name="date"
+        id="date"
+        onChange={(date, dateString) => {
+          console.log("date", date);
+          console.log("dateString", dateString);
+        }}
+      />
+    </div>
+  );
+}
